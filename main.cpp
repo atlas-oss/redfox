e1975ebe72e01477dbe44881d918e3dd30ed7039
clayy <clayy__@mailfence.com>
2017-04-27 21:37:18 +0200

    deleting awesome font dep

--- main.cpp
+++ main.cpp
@@ -19,13 +19,13 @@ int main()
 		red.battery(state, load);
 		
 	   	red.output =
-			">   "
+			">  "
 			+ ip
-			+ "  >   "
+			+ "  >  "
 			+ state
-			+ " "
+			+ "  "
 			+ std::to_string(load).substr(0,5)
-			+ "%  >   "
+			+ "%  >  "
 			+ date.substr(0, (date.length())-1);
 		XStoreName(red.dpy, red.root, red.output.c_str());
 		XFlush(red.dpy);
