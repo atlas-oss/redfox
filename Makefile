e2f350ee4b0625a5aa5a41ff20efd39efbe6542e
Florian Buestgens <florian.buestgens@web.de>
2020-10-01 11:21:48 +0200

    Moving to FreeBSD Branch, doing modifications to run on FreeBSD. Note: Not every feature is supported for now

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
-CC=g++
+CC=clang++
 
 WARN= -Wall -Wextra -Wno-unused-parameter -Wno-deprecated-declarations -Wformat-security -Wformat -Werror=format-security -Wstack-protector
 SEC= -march=native -fstack-protector-all --param ssp-buffer-size=4 -fpie -ftrapv -D_FORTIFY_SOURCE=2
 
 
-CFLAGS= ${WARN} ${SEC} -std=c++14 -O2 -fomit-frame-pointer -funroll-loops
-LDFLAGS= -lX11 -lasound -Wl,-z,relro,-z,now -pie
+CFLAGS= ${WARN} ${SEC} -I/usr/local/include -std=c++14 -O2 -fomit-frame-pointer -funroll-loops
+LDFLAGS= -L/usr/local/lib/ -lX11 -Wl,-z,relro,-z,now -pie
 
 SRC= src/redfox.cpp main.cpp
 OBJECT= redfox.o main.o
--- inc/redfox.hpp
+++ inc/redfox.hpp
@@ -12,25 +12,17 @@
 class Redfox
 {
 	int screen;
-	std::vector<std::string> path_list = {
-		"/sys/class/power_supply/BAT0/",
-		"/sys/class/power_supply/BAT1/",
-		"/sys/class/power_supply/BAT2/",
-		"/sys/class/power_supply/BAT3/"};
 
 	std::vector<std::string> full_list = {
 		"energy_full", "charge_full", "current_full"};
 	std::vector<std::string> now_list = {"energy_now", "charge_now",
 						   "current_now"};
 
-	std::string battery_path;
-
 	ifaddrs *if_addr = nullptr;
 	ifaddrs *ifa = nullptr;
 	in_addr *tmp_ptr = nullptr;
 
 	bool check_dir(const std::string dir) const;
-	bool collect_info(std::string &path) const;
 
       public:
 	Display *dpy;
@@ -45,12 +37,11 @@ class Redfox
 		}
 		screen = DefaultScreen(dpy);
 		root = RootWindow(dpy, screen);
-		collect_info(battery_path);
 	}
 
   bool date(std::string &date) const;
 	bool wifi(std::string &ip);
-	bool battery(std::string &state, double &load) const;
+	bool battery(std::string &state, int &load) const;
 	bool load_cpu_mem(double &cpu, long &mem) const;
 	bool volume(long &vol) const;
 };
